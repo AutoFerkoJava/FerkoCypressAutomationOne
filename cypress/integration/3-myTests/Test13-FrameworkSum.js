c10844bd52e06cf3905836667cb186b14007072d
AutoFerkoJava <AutoFerkoJava>
2021-11-21 16:41:08 +0100

    Test14FrameworkSumValues refactored

--- cypress/integration/3-myTests/Test13-FrameworkSum.js
+++ cypress/integration/3-myTests/Test13-FrameworkSum.js
@@ -66,14 +66,7 @@ describe("My Tenth Test Suite", function () {
     cy.get('.suggestions > ul li a').click()
     cy.get('#checkbox2').click({force: true})
     cy.get('input[type="submit"]').click()
-    
-    //cy.get('.alert').should('have.text','Success! Thank you! Your order will be delivered in next few weeks :-).') 
-    /*cy.get('.alert').then(function(element){
-      const actualText=element.text()
-      var res= actualText.split(" ")
-      res= res[1].trim()
-      expect(actualText.includes("Success")).to.be.true*/
-      
+          
     })
     
   });
